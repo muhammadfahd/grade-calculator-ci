54751724c3b456644bb254d48e91deec9a099acc
fahadkhanfahad <bashirfahad789@gmail.com>
2025-06-17 20:34:32 +0500

    changes

--- test_grade.py
+++ test_grade.py
@@ -4,7 +4,7 @@ def test_grade_A():
     assert calculate_grade(95) == "A"
 
 def test_grade_B():
-    assert calculate_grade(85) == "C"
+    assert calculate_grade(85) == "B"
 
 def test_invalid_input():
     assert calculate_grade(-5) == "Invalid marks!"
